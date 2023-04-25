7e104ff5682c8ad7f14c40e3ceda6392f4151b64
Bruno <brunolb.lafont@gmail.com>
2023-04-25 10:04:39 +0200

    add button inside post it

--- asset/script.js
+++ asset/script.js
@@ -1,8 +1,13 @@
 const createButton = document.querySelector("#createButton");
+const section = document.querySelector("section");
 
+// Button who create the postIt div
 createButton.addEventListener("click", function () {
-  const section = document.querySelector("section");
   const postIt = document.createElement("div");
-  postIt.classList.add ("postIt")
+  postIt.classList.add("postIt");
   section.appendChild(postIt);
+  const btnPostIt = document.createElement("input");
+  postIt.appendChild(btnPostIt);
+  btnPostIt.setAttribute("type", "button");
+  btnPostIt.setAttribute("id", "btnPostIt");
 });
