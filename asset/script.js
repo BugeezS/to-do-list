9de8f92f950d29211a3c402ca5f88003223ea83a
Bruno <brunolb.lafont@gmail.com>
2023-04-25 11:56:39 +0200

    add list

--- asset/script.js
+++ asset/script.js
@@ -1,13 +1,50 @@
 const createButton = document.querySelector("#createButton");
 const section = document.querySelector("section");
 
-// Button who create the postIt div
-createButton.addEventListener("click", function () {
+createButton.addEventListener("click", createPostIt);
+
+function createPostIt() {
+  const postIt = createPostItDiv();
+  section.appendChild(postIt);
+  const btnPostIt = createPostItButton();
+  postIt.appendChild(btnPostIt);
+  btnPostIt.addEventListener("click", createPostItInputText);
+  const listePostIt = createPostItUl(postIt);
+  postIt.appendChild(listePostIt);
+}
+
+function createPostItDiv() {
   const postIt = document.createElement("div");
   postIt.classList.add("postIt");
-  section.appendChild(postIt);
+  return postIt;
+}
+
+function createPostItButton() {
   const btnPostIt = document.createElement("input");
-  postIt.appendChild(btnPostIt);
   btnPostIt.setAttribute("type", "button");
   btnPostIt.setAttribute("id", "btnPostIt");
-});
+  return btnPostIt;
+}
+
+function createPostItInputText() {
+  const inputText = document.createElement("input");
+  inputText.setAttribute("type", "text");
+  inputText.setAttribute("id", "postItInputText");
+  const postIt = this.parentNode;
+  postIt.appendChild(inputText);
+}
+
+function createPostItUl(postIt) {
+  const liste = document.createElement("ul");
+  liste.classList.add("liste");
+  return liste;
+}
+
+
+
+
+  
+
+
+
+
