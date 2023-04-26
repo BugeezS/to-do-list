87e4c428f1cf30ee2e22e9324f03021a587884df
BugeezS <brunolb.lafont@gmail.com>
2023-04-26 12:10:56 +0200

    add delete

--- asset/script.js
+++ asset/script.js
@@ -3,13 +3,31 @@ const myInput = document.querySelector('#myInput');
 const myButton = document.querySelector('#myButton');
 const deleteButton = document.querySelector('#deleteButton');
 
+function addItem() {
+  const listItem = document.createElement('li');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = false;
+  listItem.appendChild(checkbox);
+  const label = document.createElement('label');
+  label.textContent = myInput.value;
+  listItem.appendChild(label);
+  myList.appendChild(listItem);
+  localStorage.setItem('myListItems', myList.innerHTML);
+  myInput.value = '';
+}
+
 function deleteCheckedItems() {
   const checkedItems = myList.querySelectorAll('input[type="checkbox"]:checked');
   checkedItems.forEach(item => {
     const listItem = item.parentNode;
     myList.removeChild(listItem);
+    localStorage.setItem('myListItems', myList.innerHTML);
   });
 }
 
 myButton.addEventListener('click', addItem);
 deleteButton.addEventListener('click', deleteCheckedItems);
+if (localStorage.getItem('myListItems')) {
+  myList.innerHTML = localStorage.getItem('myListItems');
+}
